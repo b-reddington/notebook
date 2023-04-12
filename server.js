237e35cf169448c868cc4363e1a7016cb51e8696
b-reddington <brandon.reddington98@gmail.com>
2023-04-11 23:13:04 -0700

    Pushing to heroku

--- server.js
+++ server.js
@@ -19,16 +19,13 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// GET routes
-
-
-// Note: included this along with the wildcard as a base of get operations
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+
 });
 
 // GET request for notes
@@ -38,11 +35,19 @@ app.get('/api/notes', (req, res) => {
     console.log(notes);
     // Send the notes as a JSON response
     res.json(notes);
-
     // Log our request to the terminal
     console.info(`${req.method} request received to get notes`);
 });
 
+app.post('/api/notes', (req, res) => {
+    // Destructure the request body
+    const {noteTitle, noteText } = req.body;
+    // This will create a new note
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
